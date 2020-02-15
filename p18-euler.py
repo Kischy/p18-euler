fcc40318e8a9d54f56eb7116c9fddaa8dfb2da45
Christian Kischy Haege <kischy@gmx.de>
2020-02-15 16:07:54 +0100

    Added test files and main file

---
+++ p18-euler.py
@@ -0,0 +1,23 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Mon Nov 25 20:16:13 2019
+
+@author: kisch
+"""
+
+
+
+problem_number = 18
+
+
+print("Calculation started")
+
+
+
+
+the_answer = 0
+
+
+
+print("The answer to the " + str(problem_number) + "th problem of ProjectEuler.Net is:",the_answer)
+
---
+++ p18-tests.py
@@ -0,0 +1,23 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Mon Nov 25 20:33:08 2019
+
+@author: kisch
+"""
+
+import unittest
+
+
+class TestCollatzLength(unittest.TestCase):
+    
+    def setUp(self):
+        self.N_to_W = NumberToWords()
+    
+    def test_something(self):
+        pass
+
+
+
+
+if __name__ == '__main__':
+    unittest.main(verbosity=0)
