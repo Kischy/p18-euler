adbea3148ec3821811a01b1d2081918dc2784928
Christian Kischy Haege <kischy@gmx.de>
2020-02-15 16:56:59 +0100

    Added test triangle and triangle maximum class

---
+++ triangle_maximum.py
@@ -0,0 +1,12 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Sat Feb 15 16:10:46 2020
+
+@author: kisch
+"""
+
+
+class TriangleMaximum():
+    
+    def find_triangle_maximum(triangle_data):
+        return 0
